e545d47492a533a1885d06071f65b417480993fa
Onyx Mayer <101078769+hsmayer07@users.noreply.github.com>
2023-05-12 11:10:36 -0400

    All Pre-Output Done

--- main.c
+++ main.c
@@ -3,13 +3,10 @@
 #include "counter.h" 
 
 int main(int argc, char* argv[]) {
-	if(argc < 2) {
-		fprintf(stderr, "Error: You haven't entered a filename. Please try again!\n");
+	if(argc < 6) {
+		fprintf(stderr, "Error: You haven't specified enough arguments. You must specify, in order, the input file, the output file, the number of rounds, the number of players, and the number of cards per round. \n");
 		return 1;
-	} else if(argc == 2) {
-		fprintf(stderr, "Error: You haven't entered an input and an output file. Please try again!\n");
-		return 1;
-	}
+	} 
 
 	FILE* input = fopen(argv[1], "r");
 	if(input == NULL) {
@@ -22,9 +19,19 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "Error: The input file, %s, contained a fatal formatting error. Please try again!\n", argv[1]);
 	}
 	fclose(input);
+	int rounds, players, cards; 
+	if(sscanf(argv[3], "%d", &rounds) != 1 || sscanf(argv[4], "%d", &players) != 1 || sscanf(argv[5], "%d", &cards) != 1) {
+		fprintf(stderr, "One or more of the arguments you supplied were invalid.\n");
+	}
+
 	//printf("The total size is: %d \n", inputData -> size);
 	int ***randCards = cardpicker(inputData, 3, 3, 9);
-	for(int i = 0; i < 3; i++) {
+	FILE* output = fopen(argv[2], "w");
+	if(output == NULL) {
+		fprintf(stderr, "Error: The output file, %s, couldn't be opened. Please try again! \n", argv[2]);
+	}
+
+	/*for(int i = 0; i < 3; i++) {
 		printf("\n Currently on round %d \n", i);
 		for(int j = 0; j < 3; j ++) {
 			printf("Player %d: ", j);
@@ -32,6 +39,23 @@ int main(int argc, char* argv[]) {
 				printf(" %d ", randCards[i][j][k]);
 			}
 		}
+	}*/
+	/*for(int i = 0; i < inputData->size; i++) {
+		printf("%s ", inputData -> names[i] -> data);
+		for(int j = 0; j < rounds; j++) {
+			printf("%d ", randCards[i][j][0]);
+		}
+		printf("\n");
+	}*/
+
+	for(int i = 0; i < rounds; i++) {
+		for(int j = 0; j < players; j++) {
+			for(int k = 0; k < inputData -> size; k++) {
+				printf("%d ", randCards[i][j][k]);
+			}
+			printf("\n");
+		}
+		printf("UwU \n");
 	}
 	printf("\n");
 }
